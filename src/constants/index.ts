7842ea50c979f628f4f74af233e18cd75d3a3f92
juancamg <carlingamaceras@gmail.com>
2024-12-18 18:50:30 +0100

    AOC #3 publicado. Pequeños cambios en anteriores publicaciones.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { siteConfig } from "@/config/site";
 import { Bot } from "lucide-react";
 
 export const NAV_LIST = [
-  { label: "About", path: "https://portfolio-juancamgs-projects.vercel.app/", icon: Bot },
+  { label: "Whoami", path: "https://portfolio-juancamgs-projects.vercel.app/", icon: Bot },
 ];
 
 export const SOCIALS = [
