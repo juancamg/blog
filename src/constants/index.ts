63b17ead44247ae26a03d1954e9632b73bfc0bec
juancamg <carlingamaceras@gmail.com>
2025-04-16 13:19:26 +0200

    Shards en NAV. Footprinting & Scanning CTF 1

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,12 +1,17 @@
 import { Icons } from '@/components/icons';
 import { siteConfig } from '@/config/site';
-import { Bot } from 'lucide-react';
+import { Bot, TrafficCone } from 'lucide-react';
 
 export const NAV_LIST = [
   {
     label: 'Whoami',
     path: 'https://portfolio-juancamgs-projects.vercel.app/',
     icon: Bot,
+  },
+  {
+    label: 'Shards',
+    path: 'https://xhetic-shards.vercel.app/',
+    icon: TrafficCone,
   },
 ];
 
